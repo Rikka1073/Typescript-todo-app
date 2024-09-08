a81da63f72fe51f889411a98af84eea5e761b71e
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-08 14:30:53 +0900

    ESLintの設定追加

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,20 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  {
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
