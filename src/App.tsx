fa6ac912b6c9beb8606fe59e83900e584e4456c0
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-09 22:29:32 +0900

    Chakra UIの設定確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 data-testid="title">Hello World</h1>
+      <ChakraProvider>
+        <h1 data-testid="title">Hello World</h1>
+        <Button>ボタン</Button>
+      </ChakraProvider>
     </>
   );
 }
