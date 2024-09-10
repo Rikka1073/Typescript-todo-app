2f98291f5af1a0c90c4c02c5f970f977847af3b1
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-10 23:06:39 +0900

    学習記録一覧画面作成 学習内容学習時間のinputエリア作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  ChakraProvider,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
     <>
       <ChakraProvider>
-        <h1 data-testid="title">学習記録一覧</h1>
-        <Button>ボタン</Button>
+        <Box>
+          <Heading as="h1" size="xl" data-testid="title" mt={10}>
+            学習記録一覧
+          </Heading>
+          <Box w="60%" m="auto">
+            <Box>
+              <FormControl display="flex">
+                <FormLabel
+                  fontSize="lg"
+                  display="flex"
+                  alignItems="center"
+                  m={0}
+                >
+                  学習内容
+                </FormLabel>
+                <Input type="text" w="60%" />
+              </FormControl>
+            </Box>
+            <Box mt={10}>
+              <FormControl display="flex">
+                <FormLabel
+                  fontSize="lg"
+                  display="flex"
+                  alignItems="center"
+                  m={0}
+                >
+                  学習時間
+                </FormLabel>
+                <Input type="text" w="60%" />
+              </FormControl>
+            </Box>
+          </Box>
+          <Button>登録</Button>
+        </Box>
       </ChakraProvider>
     </>
   );
