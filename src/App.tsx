b00cc4ab97b42ef67bc81f10133bbbf7a8f0ab08
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-11 07:39:26 +0900

    onChangeText　onChangeTime関数の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,55 @@
-import {
-  Box,
-  Button,
-  Center,
-  ChakraProvider,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Heading,
-  Input,
-} from "@chakra-ui/react";
+import { Box, Button, ChakraProvider, FormControl, FormLabel, Heading, Input, Text } from "@chakra-ui/react";
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [studyText, setStudyText] = useState("");
+  const [studyTime, setStudyTime] = useState("");
+
+  const onChangeText = (event) => {
+    setStudyText(event.target.value);
+  };
+
+  const onChangeTime = (event) => {
+    setStudyTime(event.target.value);
+  };
+
   return (
     <>
       <ChakraProvider>
-        <Box>
+        <Box w="60%" m="auto">
           <Heading as="h1" size="xl" data-testid="title" mt={10}>
             学習記録一覧
           </Heading>
-          <Box w="60%" m="auto">
-            <Box>
+          <Box>
+            <Box mt={10}>
               <FormControl display="flex">
-                <FormLabel
-                  fontSize="lg"
-                  display="flex"
-                  alignItems="center"
-                  m={0}
-                >
+                <FormLabel fontSize="lg" display="flex" alignItems="center" m={0}>
                   学習内容
                 </FormLabel>
-                <Input type="text" w="60%" />
+                <Input type="text" w="60%" ml={5} onChange={onChangeText} value={studyText} />
               </FormControl>
             </Box>
             <Box mt={10}>
               <FormControl display="flex">
-                <FormLabel
-                  fontSize="lg"
-                  display="flex"
-                  alignItems="center"
-                  m={0}
-                >
+                <FormLabel fontSize="lg" display="flex" alignItems="center" m={0}>
                   学習時間
                 </FormLabel>
-                <Input type="text" w="60%" />
+                <Input type="text" w="60%" ml={5} onChange={onChangeTime} value={studyTime} />
               </FormControl>
             </Box>
           </Box>
-          <Button>登録</Button>
+          <Box mt={10}>
+            <Text>入力されている学習内容 {studyText}</Text>
+          </Box>
+          <Box mt={2}>
+            <Text>入力されている学習時間 {studyTime}時間</Text>
+          </Box>
+          <Button mt={2}>登録</Button>
         </Box>
       </ChakraProvider>
     </>
   );
-}
+};
 
 export default App;
