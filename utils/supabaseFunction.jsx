cca6079300ec3e81e25ceb13521baeb6c32d891c
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-11 21:58:22 +0900

    関数の修正

--- utils/supabaseFunction.jsx
+++ utils/supabaseFunction.jsx
@@ -1,6 +1,6 @@
 import { supabase } from "./supabase";
 
-export const getAllTodos = async = () => {
+export const getAllTodos = async () => {
   const todos = await supabase.from("study-record").select("*");
   console.log(todos.data);
 };
