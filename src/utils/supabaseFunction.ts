e358fa80d61e5004033673529b3527d055a6faa3
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-13 06:33:36 +0900

    todosDataの追加

--- src/utils/supabaseFunction.ts
+++ src/utils/supabaseFunction.ts
@@ -1,6 +1,13 @@
+import { Todo } from "../domain/todo";
 import { supabase } from "./supabase";
 
-export const getAllTodos = async () => {
+export const getAllTodos = async (): Promise<Todo[]> => {
   const todos = await supabase.from("study-record").select("*");
   console.log(todos.data);
+
+  const todosData = todos.data.map((todo) => {
+    return new Todo(todo.id, todo.title, todo.time);
+  });
+
+  return todosData;
 };
