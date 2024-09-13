5caf705eb719f85b9f50e70d968ff99235fa6e17
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-14 07:24:17 +0900

    css修正 button追加

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App = () => {
   return (
     <>
       <ChakraProvider>
-        <Box w="60%" m="auto" bg="gray.100">
+        <Box w="60%" m="auto" bg="gray.100" p={10}>
           <Center>
             <Heading as="h1" size="xl" data-testid="title" mt={10}>
               学習記録一覧
@@ -61,22 +61,33 @@ const App = () => {
               </FormControl>
             </Box>
           </Box>
-          <Box mt={10}>
-            <Text>入力されている学習内容 {studyText}</Text>
+          <Box mt={10} bg="white" pt={5} pb={5} borderRadius="10">
+            <Text ml={5}>入力されている学習内容 {studyText}</Text>
           </Box>
-          <Box mt={2}>
-            <Text>入力されている学習時間 {studyTime}時間</Text>
+          <Box mt={5} bg="white" pt={5} pb={5} borderRadius="10">
+            <Text ml={5}>入力されている学習時間 {studyTime}時間</Text>
           </Box>
-          <Button onClick={onClickAddTodo} mt={2}>
-            登録
-          </Button>
+          <Center>
+            <Button onClick={onClickAddTodo} mt={2} colorScheme="purple" w="50%" borderRadius="999" m="5">
+              登録
+            </Button>
+          </Center>
           <Box mt={10}>
             {records.map((record) => {
               return (
-                <Box key={record.id}>
-                  <Text>{record.title}</Text>
-                  <Text>{record.time}</Text>
-                </Box>
+                <>
+                  <Box bg="white" p={10} mb={5} borderRadius="10" display="flex" justifyContent="space-between">
+                    <Box key={record.id} display="flex">
+                      <Text mr={5} display="flex" alignItems="center">
+                        {record.title}
+                      </Text>
+                      <Text display="flex" alignItems="center">
+                        {record.time}時間
+                      </Text>
+                    </Box>
+                    <Button>削除</Button>
+                  </Box>
+                </>
               );
             })}
           </Box>
