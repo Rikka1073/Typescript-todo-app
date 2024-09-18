f0dfb6d7deaecaaf96e21be1bee2b749d2db2ab6
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-19 07:50:24 +0900

    mock関数の設定

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,10 +1,12 @@
 import App from "../App";
 import { render, screen } from "@testing-library/react";
+import { Todo } from "../domain/todo";
+
+const mockGetAllTodos = jest.fn().mockResolvedValue([new Todo("1", "test1", 1), new Todo("2", "test2", 2), new Todo("3", "test3", 3), new Todo("4", "test4", 4)]);
 
 describe("App", () => {
   it("タイトルがあること", () => {
     render(<App />);
-
     const title = screen.getByTestId("title");
     expect(title).toBeInTheDocument();
     screen.debug();
